ffb99a63a895d8836cbc789f917f3785af929684
leehoyong <lee199402@gmail.com>
2019-06-01 02:19:48 +0900

    - add main fragment data binding

--- app/src/main/java/com/da62/ui/MainFragment.kt
+++ app/src/main/java/com/da62/ui/MainFragment.kt
@@ -1,9 +1,28 @@
 package com.da62.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.da62.databinding.FragmentMainBinding
 import com.da62.ui.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : BaseFragment() {
 
     private val viewModel by viewModel<MainViewModel>()
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false).apply {
+            viewModel = this@MainFragment.viewModel
+            lifecycleOwner = this@MainFragment
+        }
+
+        return binding.root
+    }
 }
