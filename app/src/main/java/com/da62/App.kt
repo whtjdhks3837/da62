8ad4f96595ec93c9c8ca044a979c883eddd09622
joebang <joebang@smartstudy.co.kr>
2019-06-05 00:34:06 +0900

    Application 카카오sdk 초기화

--- app/src/main/java/com/da62/App.kt
+++ app/src/main/java/com/da62/App.kt
@@ -2,18 +2,38 @@ package com.da62
 
 import android.app.Application
 import com.da62.datasource.di.appModules
+import com.kakao.auth.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class App : Application(){
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        KakaoSDK.init(kakaoAdapter)
         startKoin {
             androidContext(this@App)
 
             modules(appModules)
+        }
+    }
+
+    private val kakaoAdapter = object : KakaoAdapter() {
+        override fun getApplicationConfig() = IApplicationConfig { applicationContext }
+
+        override fun getSessionConfig(): ISessionConfig {
+            return object : ISessionConfig {
+                override fun isSaveFormData() = true
+
+                override fun getAuthTypes() = Array(1) { AuthType.KAKAO_LOGIN_ALL }
+
+                override fun isSecureMode() = false
+
+                override fun getApprovalType(): ApprovalType? = ApprovalType.INDIVIDUAL
+
+                override fun isUsingWebviewTimer() = false
+            }
         }
     }
 }
