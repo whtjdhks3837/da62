7e263f388c20766e7f0d95cdf46267f6f42dc8c7
leehoyong <lee199402@gmail.com>
2019-06-07 16:41:17 +0900

    main event listener 추가

--- app/src/main/java/com/da62/presenter/main/MainViewModel.kt
+++ app/src/main/java/com/da62/presenter/main/MainViewModel.kt
@@ -3,6 +3,12 @@ package com.da62.presenter.main
 import com.da62.presenter.base.BaseViewModel
 import com.da62.usecase.MainUseCase
 
-class MainViewModel(private val useCase: MainUseCase) : BaseViewModel() {
+class MainViewModel(
+    private val useCase: MainUseCase
+) : BaseViewModel(), MainEventListener {
+
+}
+
+interface MainEventListener {
 
 }
