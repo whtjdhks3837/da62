a9ea704ce794a454887a585aad56856d792ddebb
leehoyong <lee199402@gmail.com>
2019-06-07 16:47:00 +0900

    main recyclerview adapter  추가

--- app/src/main/java/com/da62/presenter/main/MainActivity.kt
+++ app/src/main/java/com/da62/presenter/main/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.da62.presenter.main
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.da62.R
+import com.da62.databinding.ActivityMainBinding
 import com.da62.presenter.base.BaseActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity() {
 
+    private val viewModel by viewModel<MainViewModel>()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        binding.mainRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            setHasFixedSize(true)
+            adapter = MainAdapter(viewModel, this@MainActivity)
+        }
     }
 }
