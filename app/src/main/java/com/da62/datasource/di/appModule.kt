63f85896e54cb8349d844ca7e6c3866c2b0eeb38
joebang <joebang@smartstudy.co.kr>
2019-06-15 00:44:41 +0900

    모듈 리스트 api모듈 추가

--- app/src/main/java/com/da62/datasource/di/appModule.kt
+++ app/src/main/java/com/da62/datasource/di/appModule.kt
@@ -1,8 +1,9 @@
 package com.da62.datasource.di
 
+import com.da62.datasource.api.apiModules
 import com.da62.presenter.viewModelModules
 import com.da62.repository.repositoryModules
 import com.da62.usecase.useCaseModules
 
 
-val appModules = listOf(viewModelModules, repositoryModules, useCaseModules)
+val appModules = listOf(apiModules, viewModelModules, repositoryModules, useCaseModules)
