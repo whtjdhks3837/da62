e78d2bc07d7f4fe0f55d261ad457896f148ea93d
joebang <joebang@smartstudy.co.kr>
2019-06-15 00:43:39 +0900

    ApiService 추가

--- app/src/main/java/com/da62/datasource/api/ApiService.kt
+++ app/src/main/java/com/da62/datasource/api/ApiService.kt
@@ -1,5 +1,27 @@
 package com.da62.datasource.api
 
+import com.da62.model.KakaoProfile
+import com.da62.model.KakaoTalkProfile
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+
 interface ApiService {
 
+    @POST("post the id to server")
+    fun postKakaoId(): Single<Any>
+}
+
+interface KakaoApiService {
+
+    @GET("/v1/api/talk/profile")
+    fun getKakaoTalkProfile(
+        @Header("Authorization") accessToken: String
+    ): Single<KakaoTalkProfile>
+
+    @POST("/v2/user/me")
+    fun postKakaoProfile(
+        @Header("Authorization") accessToken: String
+    ): Single<KakaoProfile>
 }
