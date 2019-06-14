3abd3a72302251396df9eafb3e96d0f0f7079798
joebang <joebang@smartstudy.co.kr>
2019-06-15 00:43:55 +0900

    Api 모듈 추가

--- app/src/main/java/com/da62/datasource/api/Api.kt
+++ app/src/main/java/com/da62/datasource/api/Api.kt
@@ -1,5 +1,44 @@
 package com.da62.datasource.api
 
-object Api {
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
+val apiModules = module {
+    single<ApiService> {
+        Retrofit.Builder()
+            .client(get())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://kapi.kakao.com")
+            .build()
+            .create()
+    }
+
+    single<KakaoApiService> {
+        Retrofit.Builder()
+            .client(get())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://kapi.kakao.com")
+            .build()
+            .create()
+    }
+
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(get())
+            .build()
+    }
+
+    single {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        } as Interceptor
+    }
 }
