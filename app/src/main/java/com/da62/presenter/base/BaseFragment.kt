03cebf2707a4f0227c8d6336e1898233334b8606
joebang <joebang@smartstudy.co.kr>
2019-06-26 12:02:45 +0900

    BaseFragment 수정

--- app/src/main/java/com/da62/presenter/base/BaseFragment.kt
+++ app/src/main/java/com/da62/presenter/base/BaseFragment.kt
@@ -1,6 +1,24 @@
 package com.da62.presenter.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import org.jetbrains.anko.AnkoLogger
 
-open class BaseFragment : Fragment(), AnkoLogger
+abstract class BaseFragment<T : ViewDataBinding> : Fragment(), AnkoLogger {
+
+    lateinit var mBinding: T
+
+    abstract val mResourceId: Int
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        DataBindingUtil.inflate<T>(
+            LayoutInflater.from(context), mResourceId, container, false
+        ).apply {
+            mBinding = this
+            mBinding.lifecycleOwner = this@BaseFragment
+        }.root
+}
