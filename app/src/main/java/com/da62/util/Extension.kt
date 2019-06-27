ad06f08d2ce05505576c182c5045a154ecdb8d7d
leehoyong <lee199402@gmail.com>
2019-06-28 01:25:53 +0900

    - 메인 recyclerview item design 수정

--- app/src/main/java/com/da62/util/Extension.kt
+++ app/src/main/java/com/da62/util/Extension.kt
@@ -1,6 +1,12 @@
 package com.da62.util
 
+import android.content.Context
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
 infix fun CompositeDisposable.add(d: Disposable) = this.add(d)
+
+fun dp2px(context: Context, dpValue: Float): Int {
+    val scale = context.resources.displayMetrics.density
+    return (dpValue * scale + 0.5f).toInt()
+}
