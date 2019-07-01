cec3959bb9be182c0e3e96d968880d0eea8e93c8
leehoyong <lee199402@gmail.com>
2019-07-01 23:02:08 +0900

    - add detail view top back pressed button click event

--- app/src/main/java/com/da62/presenter/detail/DetailActivity.kt
+++ app/src/main/java/com/da62/presenter/detail/DetailActivity.kt
@@ -2,6 +2,9 @@ package com.da62.presenter.detail
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import com.da62.R
 import com.da62.databinding.ActivityDetailBinding
 import com.da62.presenter.base.BaseActivity
@@ -18,5 +21,36 @@ class DetailActivity : BaseActivity() {
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+
+        binding.detailHorizontalRecyclerView.apply {
+            layoutManager =
+                LinearLayoutManager(this@DetailActivity, LinearLayoutManager.HORIZONTAL, false)
+            adapter = DetailInfoAdapter(this@DetailActivity)
+            addItemDecoration(
+                DetailItemDecoration(
+                    dp2px(this@DetailActivity, 24f),
+                    dp2px(this@DetailActivity, 16f)
+                )
+            )
+        }
+
+        binding.detailGalleryRecyclerView.apply {
+            layoutManager =
+                LinearLayoutManager(this@DetailActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
+
+        val infoSnapHelper = LinearSnapHelper()
+        infoSnapHelper.attachToRecyclerView(binding.detailHorizontalRecyclerView)
+
+        viewModel.clickToBack.observe(this, Observer {
+           supportFinishAfterTransition()
+        })
+
+    }
+
+    // transition complete
+    override fun onEnterAnimationComplete() {
+        super.onEnterAnimationComplete()
+
     }
 }
--- app/src/main/java/com/da62/presenter/detail/DetailViewModel.kt
+++ app/src/main/java/com/da62/presenter/detail/DetailViewModel.kt
@@ -2,7 +2,24 @@ package com.da62.presenter.detail
 
 import com.da62.presenter.base.BaseViewModel
 import com.da62.usecase.DetailUseCase
+import com.da62.util.SingleLiveEvent
 
 class DetailViewModel(private val useCase: DetailUseCase) : BaseViewModel() {
 
+    private val _plantInfoList = MutableLiveData<List<Plant>>()
+    val plantInfoList: LiveData<List<Plant>>
+        get() = _plantInfoList
+
+    private val _clickToBack = SingleLiveEvent<Any>()
+    val clickToBack: LiveData<Any>
+        get() = _clickToBack
+
+    init {
+
+        _plantInfoList.value = useCase.getInfoList()
+    }
+
+    fun clickToBack() {
+        _clickToBack.call()
+    }
 }
