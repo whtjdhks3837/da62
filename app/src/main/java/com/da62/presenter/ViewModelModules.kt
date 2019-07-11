0910cbdf2a355cc22ba24df442c6ee4cfdc9eedb
joebang <joebang@smartstudy.co.kr>
2019-07-12 00:35:45 +0900

    di 수정

--- app/src/main/java/com/da62/presenter/ViewModelModules.kt
+++ app/src/main/java/com/da62/presenter/ViewModelModules.kt
@@ -4,7 +4,6 @@ import com.da62.presenter.detail.DetailViewModel
 import com.da62.presenter.login.LoginViewModel
 import com.da62.presenter.main.MainViewModel
 import com.da62.presenter.splash.SplashViewModel
-import com.da62.presenter.write.WriteViewModel
 import com.da62.presenter.write.plant.PlantRegistViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -13,7 +12,6 @@ val viewModelModules = module {
     viewModel { SplashViewModel() }
     viewModel { LoginViewModel(get()) }
     viewModel { MainViewModel(get()) }
-    viewModel { WriteViewModel(get()) }
     viewModel { DetailViewModel(get()) }
-    viewModel { PlantRegistViewModel() }
+    viewModel { PlantRegistViewModel(get()) }
 }
