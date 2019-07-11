456ee526f48f633a1a1a4d3fe527f8171b1f96f6
joebang <joebang@smartstudy.co.kr>
2019-07-12 00:39:01 +0900

    식물 물주기 달력 / 타임피커 다이얼로그 작성

---
+++ app/src/main/java/com/da62/presenter/write/plant/dialog/PlantWaterCalendarDialog.kt
@@ -0,0 +1,31 @@
+package com.da62.presenter.write.plant.dialog
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.da62.R
+import com.da62.databinding.DialogWaterCalendarBinding
+
+class PlantWaterCalendarDialog(private val callback: (String) -> Unit) : DialogFragment() {
+
+    private lateinit var binding: DialogWaterCalendarBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        DataBindingUtil.inflate<DialogWaterCalendarBinding>(
+            inflater, R.layout.dialog_water_calendar, container, false
+        ).apply {
+            binding = this
+        }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.calendar.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            callback.invoke("$year.$month.$dayOfMonth")
+            dismiss()
+        }
+    }
+}
