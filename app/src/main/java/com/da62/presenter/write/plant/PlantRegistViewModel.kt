dad72019e9e52008eb483218bf0abfc357fbde79
baejang <qowod77@naver.com>
2019-07-22 00:13:46 +0900

    뷰모델 수정

--- app/src/main/java/com/da62/presenter/write/plant/PlantRegistViewModel.kt
+++ app/src/main/java/com/da62/presenter/write/plant/PlantRegistViewModel.kt
@@ -4,10 +4,14 @@ import android.view.View
 import android.widget.CompoundButton
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.da62.datasource.local.LoginLocalDataSourceImpl
+import com.da62.model.SavePlant
 import com.da62.presenter.base.BaseViewModel
+import com.da62.presenter.write.plant.fragment.Card
 import com.da62.usecase.PlantRegistUseCase
 import com.da62.util.SingleLiveEvent
 import com.da62.util.add
+import retrofit2.HttpException
 
 class PlantRegistViewModel(private val useCase: PlantRegistUseCase) : BaseViewModel() {
 
@@ -19,9 +23,9 @@ class PlantRegistViewModel(private val useCase: PlantRegistUseCase) : BaseViewMo
 
     val next: LiveData<Any> = _next
 
-    private val _speciesImage = MutableLiveData<View?>()
+    private val _speciesImage = MutableLiveData<Card>()
 
-    val speciesImage: LiveData<View?> = _speciesImage
+    val speciesImage: LiveData<Card> = _speciesImage
 
     val species = SingleLiveEvent<String>()
 
@@ -29,48 +33,51 @@ class PlantRegistViewModel(private val useCase: PlantRegistUseCase) : BaseViewMo
 
     val searchResult: LiveData<List<String>> = _searchResult
 
+    private val _isSpeciesSearching = SingleLiveEvent<Boolean>()
+
+    val isSpeciesSearching: LiveData<Boolean> = _isSpeciesSearching
+
     val nickName = SingleLiveEvent<String>()
 
-    private val _plantLevel = SingleLiveEvent<Int>()
+    private val _plantLevel = SingleLiveEvent<String>()
 
-    val plantLevel: LiveData<Int> = _plantLevel
+    private val plantLevel: LiveData<String> = _plantLevel
 
-    val waterDate = SingleLiveEvent<String>().apply {
-        value = ""
+    val waterDate = SingleLiveEvent<String>()
+
+    val waterOften = SingleLiveEvent<String>().apply {
+        value = "0"
     }
 
-    val waterOften = SingleLiveEvent<Int>().apply {
-        value = 0
-    }
-
-    val waterTime = SingleLiveEvent<String>().apply {
-        value = ""
-    }
+    val waterTime = SingleLiveEvent<String>()
 
     val isWaterAlarmChecked = SingleLiveEvent<Boolean>().apply {
         value = false
     }
 
     fun inputSpeciesText(input: String) {
-        compositeDisposable add useCase.getSpeciesTextList(input)
-            .subscribe({
-                _searchResult.value = it
-            }, {
-                it.printStackTrace()
-            })
-    }
-
-    fun onNextClick(view: View) {
-        _next.call()
+        getUser()?.let { user ->
+            _isSpeciesSearching.postValue(true)
+            compositeDisposable add useCase.getPlantNames(user.token, input)
+                .subscribe({
+                    _isSpeciesSearching.value = false
+                    _searchResult.value = it
+                }, {
+                    _isSpeciesSearching.value = false
+                    it.printStackTrace()
+                })
+        } ?: error("인증실패입니다.")
     }
 
     fun onSpeciesImageChecked(button: CompoundButton, isChecked: Boolean) {
-        if (!isChecked) {
-            if (_speciesImage.value?.id == button.id) {
-                _speciesImage.value = null
-            }
+        if (isChecked) {
+            _speciesImage.value = button.tag as Card
         } else {
-            _speciesImage.value = button
+            _speciesImage.value?.let {
+                if (it == button.tag) {
+                    _speciesImage.value = null
+                }
+            }
         }
     }
 
@@ -110,31 +117,79 @@ class PlantRegistViewModel(private val useCase: PlantRegistUseCase) : BaseViewMo
     private fun levelValidate() =
         _plantLevel.value?.let { true } ?: false
 
-    fun setPlantLevel(id: Int) {
-        _plantLevel.value = id
+    fun setPlantLevel(level: String) {
+        _plantLevel.value = level
     }
 
     fun onWaterFragmentCompleteClick(view: View) {
-        if (waterDateValidate()) {
+        if (!waterDateValidate()) {
             _error.value = "날짜를 입력해주세요."
             return
         }
-        if (waterOftenValidate()) {
+        if (!waterOftenValidate()) {
             _error.value = "주기를 입력해주세요."
             return
         }
-        if (waterTimeValidate()) {
+        if (!waterTimeValidate()) {
             _error.value = "시간을 입력해주세요."
             return
         }
+        save()
     }
 
     private fun waterDateValidate() =
-        waterDate.value?.isNotEmpty() ?: throw IllegalStateException()
+        waterDate.value?.let { true } ?: false
 
     private fun waterOftenValidate() =
-        waterOften.value?.let { it > 0 } ?: throw IllegalStateException()
+        waterOften.value?.let { it.toInt() > 0 } ?: throw IllegalStateException()
 
     private fun waterTimeValidate() =
-        waterTime.value?.isNotEmpty() ?: throw IllegalStateException()
+        waterTime.value?.let { true } ?: false
+
+    private fun save() {
+        getUser()?.let { user ->
+            compositeDisposable add useCase.savePlant(
+                user.token,
+                SavePlant(
+                    alarm = false,
+                    card = speciesImage.value!!.name,
+                    grow = plantLevel.value!!,
+                    kind = species.value!!,
+                    name = nickName.value!!,
+                    raiseDate = toLocalDateTimeFormat(),
+                    waterDate = waterOften.value!!.toInt(),
+                    waterTime = toLocalDateTimeFormat()
+                ),
+                user.userId
+            ).subscribe({
+                _next.call()
+            }, {
+                it.printStackTrace()
+                _error.value = "서버오류입니다. 다시 시도해주세요."
+            })
+        } ?: error("로그인 오류입니다.")
+    }
+
+    private fun toLocalDateTimeFormat(): String {
+        val yearMonthDay = waterDate.value!!.split(".")
+        val hourMin = waterTime.value!!.split(":")
+        val year = yearMonthDay[0]
+        val month = yearMonthDay[1]
+        val day = yearMonthDay[2]
+        val hour = hourMin[0]
+        val min = hourMin[1]
+        return "$year-${addZero(month)}-${addZero(day)}T${addZero(hour)}:${addZero(min)}:00Z"
+    }
+
+    private fun addZero(value: String) = if (value.length == 1) {
+        "0$value"
+    } else {
+        value
+    }
+
+    private fun getUser() = LoginLocalDataSourceImpl.user
+
+    private fun error(message: String) {
+        _error.value = message
+    }
 }
