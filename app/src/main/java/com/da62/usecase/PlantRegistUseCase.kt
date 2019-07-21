b1bef070a615a23bedd93cce5ca57be139b366fc
baejang <qowod77@naver.com>
2019-07-22 00:14:09 +0900

    유즈케이스 / 레포

--- app/src/main/java/com/da62/usecase/PlantRegistUseCase.kt
+++ app/src/main/java/com/da62/usecase/PlantRegistUseCase.kt
@@ -1,15 +1,22 @@
 package com.da62.usecase
 
+import com.da62.model.SavePlant
 import com.da62.repository.PlantRegistRepository
+import io.reactivex.Observable
 import io.reactivex.Single
 
 interface PlantRegistUseCase {
 
-    fun getSpeciesTextList(input: String): Single<List<String>>
+    fun getPlantNames(token: String, input: String): Observable<List<String>>
+
+    fun savePlant(token: String, savePlant: SavePlant, userId: Int): Single<String>
 }
 
 class PlantRegistUseCaseImpl(private val repository: PlantRegistRepository) : PlantRegistUseCase {
 
-    override fun getSpeciesTextList(input: String) =
-        repository.getSpeciesTextList(input)
+    override fun getPlantNames(token: String, input: String): Observable<List<String>> =
+        repository.getPlantNames(token, input)
+
+    override fun savePlant(token: String, savePlant: SavePlant, userId: Int): Single<String> =
+        repository.savePlant(token, savePlant, userId)
 }
